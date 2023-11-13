98fb1f9296e9c840eca961b2dc845ed2ba12dcc2
satheesh <satheeshn001@gmail.com>
2023-11-13 15:53:27 +0530

    Commit

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,6 +1,28 @@
+import java.util.Arrays;
+
 public class MyClass {
+
+    public void addTwoNumbers(int x, int y) {
+        System.out.println(x + y);
+        return;
+    }
+
+    // want to write code for sorting an array
+    public void sortArray(int[] array) {
+        // sorting array
+        Arrays.sort(array);
+        //want to print array
+        System.out.println(Arrays.toString(array));
+        return;
+
+    }
+
     public static void main(String[] args) {
         // Your Java code here
         System.out.println("Hello wold");
+        MyClass myClass = new MyClass();
+        myClass.addTwoNumbers(10, 20);
+        myClass.sortArray(new int[]{110, 2, 3});
+
     }
 }
--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -1,19 +1,22 @@
 public class PalindromeChecker {
     public static void main(String[] args) {
-        String s = "aaaaaaaaaaaaaaaaaaa";
+        String s = "sas";
         System.out.println(isPalindrome(s));
         System.out.println(s);
+        System.out.println(s.length());
     }
 
     public static boolean isPalindrome(String s) {
         if (s.length() == 0)
         {
+            System.out.println("Length of the word is 0 ");
             return true;
         }
         int i = 0;
         int j = s.length() - 1;
         while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
+            if (s.charAt(i) != s.charAt(j))
+            {
                 return false;
             }
             i++;
